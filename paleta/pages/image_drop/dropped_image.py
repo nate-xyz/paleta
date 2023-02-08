0c6c886de7a2ec25a838029adbd0c064bfb80468
nate-xyz <nxyz0000@gmail.com>
2023-02-08 17:07:29 -0500

    use pixbuf new_from_file

--- paleta/pages/image_drop/dropped_image.py
+++ paleta/pages/image_drop/dropped_image.py
@@ -1,6 +1,9 @@
 from gi.repository import Adw, GLib, Gtk, GdkPixbuf
 from PIL import Image
 
+import os
+import html
+
 class DroppedImage(Adw.Bin):
     __gtype_name__ = 'DroppedImage'
 
@@ -12,12 +15,15 @@ class DroppedImage(Adw.Bin):
 
     def load_image(self, image_path):
         self.image_path = image_path
-        source_image = Image.open(image_path)
-        width, height = source_image.size
-        bytes_ = source_image.tobytes("raw", "RGBA")
-        glib_bytes = GLib.Bytes.new(bytes_)
-        pixbuf = GdkPixbuf.Pixbuf.new_from_bytes(glib_bytes, GdkPixbuf.Colorspace.RGB, True, 8, width, height, width*4)
-
+        self.image_name = html.escape(os.path.basename(image_path))
+        try:
+            pixbuf = GdkPixbuf.Pixbuf.new_from_file(image_path)
+        except Exception as e:
+            source_image = Image.open(image_path)
+            width, height = source_image.size
+            bytes_ = source_image.tobytes("raw", "RGBA")
+            glib_bytes = GLib.Bytes.new(bytes_)
+            pixbuf = GdkPixbuf.Pixbuf.new_from_bytes(glib_bytes, GdkPixbuf.Colorspace.RGB, True, 8, width, height, width*4)
         self.picture = Gtk.Picture.new_for_pixbuf(pixbuf)
         self.set_child(self.picture)
 
